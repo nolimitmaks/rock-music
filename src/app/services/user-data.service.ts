fcc1cf059aca6c2303354e5c6e6ecc2a3b0fa360
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2021-03-03 02:33:16 -0800

    add user data service & user model

---
+++ src/app/services/user-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
+import { User } from '../models/model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+
+  constructor() { }
+
+  
+  getCurrentUser(): Observable<User> {
+
+    return of(
+      {
+        name: 'Maks',
+        favoriteBandId: 'a2a6c014-35e7-478f-b4a1-fc83dcfe811x'
+        
+      }
+    ).pipe(
+      tap(() => console.log('start fetching user data')),
+      delay(1000),
+      tap(() => console.log('finished fetching user data'))
+    )
+  }
+}
